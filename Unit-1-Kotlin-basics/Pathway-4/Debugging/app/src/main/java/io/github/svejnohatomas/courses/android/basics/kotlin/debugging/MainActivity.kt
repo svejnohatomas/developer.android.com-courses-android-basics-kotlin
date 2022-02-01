fbe1031284bedf008b6e93475a2c3f7960c040e1
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2022-02-01 12:06:34 +0000

    Add Android Debugging app

---
+++ Unit-1-Kotlin-basics/Pathway-4/Debugging/app/src/main/java/io/github/svejnohatomas/courses/android/basics/kotlin/debugging/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.svejnohatomas.courses.android.basics.kotlin.debugging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        logging()
+    }
+
+    fun logging() {
+        Log.v(TAG, "Hello, world!")
+    }
+}
