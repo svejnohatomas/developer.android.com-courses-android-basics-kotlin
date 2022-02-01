754b79f65fe436e622ff8a54d1f0b4331cae2a91
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2022-02-01 16:17:45 +0000

    Add application logic for Tip Time

--- Unit-2-Layouts/Pathway-1/TipTime/app/src/main/java/io/github/svejnohatomas/courses/android/basics/kotlin/tiptime/MainActivity.kt
+++ Unit-2-Layouts/Pathway-1/TipTime/app/src/main/java/io/github/svejnohatomas/courses/android/basics/kotlin/tiptime/MainActivity.kt
@@ -1,11 +1,44 @@
 package io.github.svejnohatomas.courses.android.basics.kotlin.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.svejnohatomas.courses.android.basics.kotlin.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val costOfServiceText = binding.costOfService.text.toString()
+
+        val cost = costOfServiceText.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
